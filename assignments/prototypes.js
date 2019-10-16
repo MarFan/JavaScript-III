7925bd708193bd61f40eb9062ec8d8216235cb97
Mr. Jonah <jaitch@gmail.com>
2019-10-16 17:40:09 -0500

    MVP

--- assignments/prototypes.js
+++ assignments/prototypes.js
@@ -1,9 +1,11 @@
 /*
-  Object oriented design is commonly used in video games.  For this part of the assignment you will be implementing several constructor functions with their correct inheritance hierarchy.
+  Object oriented design is commonly used in video games.  For this part of the assignment you will be implementing several constructor 
+  functions with their correct inheritance hierarchy.
 
   In this file you will be creating three constructor functions: GameObject, CharacterStats, Humanoid.  
 
-  At the bottom of this file are 3 objects that all end up inheriting from Humanoid.  Use the objects at the bottom of the page to test your constructor functions.
+  At the bottom of this file are 3 objects that all end up inheriting from Humanoid.  
+  Use the objects at the bottom of the page to test your constructor functions.
   
   Each constructor function has unique properties and methods that are defined in their block comments below:
 */
@@ -16,12 +18,34 @@
   * destroy() // prototype method that returns: `${this.name} was removed from the game.`
 */
 
+function GameObject(attributes) {
+  this.createdAt = attributes.createdAt;
+  this.name = attributes.name;
+  this.dimensions = attributes.dimensions;
+}
+
+GameObject.prototype.destroy = function(){
+  return `${this.name} was removed from the game.`;
+}
+
 /*
   === CharacterStats ===
   * healthPoints
   * takeDamage() // prototype method -> returns the string '<object name> took damage.'
   * should inherit destroy() from GameObject's prototype
 */
+
+function CharacterStats(attributes) {
+  GameObject.call(this, attributes),
+  this.healthPoints = attributes.healthPoints;
+  this.attackPoints = attributes.attackPoints;
+  this.defencePoints = attributes.defencePoints;
+}
+
+CharacterStats.prototype = Object.create(GameObject.prototype);
+CharacterStats.prototype.takeDamage = function()  {
+  return `${this.name} took damage`;
+}
 
 /*
   === Humanoid (Having an appearance or character resembling that of a human.) ===
@@ -33,6 +57,18 @@
   * should inherit takeDamage() from CharacterStats
 */
  
+function Humanoid(attributes) {
+  CharacterStats.call(this, attributes);  
+  this.team = attributes.team;
+  this.weapons = attributes.weapons;
+  this.language = attributes.language;
+}
+
+Humanoid.prototype = Object.create(CharacterStats.prototype);
+Humanoid.prototype.greet = function() {
+  return `${this.name} offers a greeting in ${this.language}`;
+}
+
 /*
   * Inheritance chain: GameObject -> CharacterStats -> Humanoid
   * Instances of Humanoid should have all of the same properties as CharacterStats and GameObject.
@@ -41,7 +77,7 @@
 
 // Test you work by un-commenting these 3 objects and the list of console logs below:
 
-/*
+
   const mage = new Humanoid({
     createdAt: new Date(),
     dimensions: {
@@ -102,9 +138,66 @@
   console.log(archer.greet()); // Lilith offers a greeting in Elvish.
   console.log(mage.takeDamage()); // Bruce took damage.
   console.log(swordsman.destroy()); // Sir Mustachio was removed from the game.
-*/
+
 
   // Stretch task: 
   // * Create Villain and Hero constructor functions that inherit from the Humanoid constructor function.  
   // * Give the Hero and Villains different methods that could be used to remove health points from objects which could result in destruction if health gets to 0 or drops below 0;
   // * Create two new objects, one a villain and one a hero and fight it out with methods!
+
+  function Villian(attributes) {
+    Humanoid.call(this, attributes);
+  }
+
+  Villian.prototype.attack = function(){
+    return Math.floor(Math.random() * this.attackPoints + 1);
+  }
+ 
+  function Hero(attributes) {
+    Humanoid.call(this, attributes);
+  }
+
+  Hero.prototype.attack = function(){
+    return Math.floor(Math.random() * this.attackPoints + 1);
+  }
+
+  const badGuy = new Villian({
+    createdAt: new Date(),
+    dimensions: {
+        length: 2,
+        width: 3,
+        height: 6
+    },
+    healthPoints: 25,
+    attackPoints: 8,
+    defencePoints: 12,
+    name: 'Bowser',
+    team: 'Koopa',
+    weapons: [
+      'Shell',
+      'Spikes'
+    ],
+    language: 'Toddler'
+  });
+
+  const goodGuy = new Hero({
+    createdAt: new Date(),
+    dimensions: {
+      length: 1,
+      width: 2,
+      height: 3
+    },
+    healthPoints: 12,
+    attackPoints: 8,
+    defencePoints: 8,
+    name: 'Luigi',
+    team: 'Bros.',
+    weapons: [
+      'Speed',
+      'Fireballs'
+    ],
+    language: 'Italian'
+  });
+
+  console.log(goodGuy)
+
--- assignments/this.js
+++ assignments/this.js
@@ -1,10 +1,10 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. 
-* 2. 
-* 3. 
-* 4. 
+* 1. window/global - if a funciton is in the global scope, `this` will be the window object
+* 2. implicit - the scope of `this` is contained to the object that is called to the left of the dot.
+* 3. explicit - `this` scope is directly tied to the specific data passed in to a constructor function
+* 4. new - `this` is applied to the data that is used to create a new object.
 *
 * write out a code example of each explanation above
 */
@@ -12,15 +12,57 @@
 // Principle 1
 
 // code example for Window Binding
+function windowBinding(name){
+    console.log(`Hello, ${name}.`);
+    console.log(this)
+}
+console.log(windowBinding);
 
 // Principle 2
 
 // code example for Implicit Binding
+const implicitBinding = {
+    favVehicle: 'VW Beetle',
+    sayHello: function(name) {
+        console.log(`${this.favVehicle} is ${name} favorite car.`);
+        console.log(this)
+    }
+}
+
+implicitBinding.sayHello('Jonah\'s');
 
 // Principle 3
 
 // code example for New Binding
+function Car(attributes) {
+    this.year = attributes.year,
+    this.make = attributes.make,
+    this.model = attributes.model,
+    this.driver = attributes.driver,
+    this.brag = function() {
+        console.log(`${this.driver}, drives a ${this.year} ${this.make} ${this.model}`);
+    }
+}
+
+const newCar = new Car({
+    year: 1968,
+    make: 'Volkswagen',
+    model: 'Beetle',
+    driver: 'Jonah'
+});
+
+const newCar2 = new Car({
+    year: 2008,
+    make: 'Honda',
+    model: 'Odyssey',
+    driver: 'Laura'
+});
+
+newCar.brag();
+newCar2.brag();
 
 // Principle 4
 
 // code example for Explicit Binding
+newCar.brag.call(newCar2);
+newCar.brag.call(newCar);
